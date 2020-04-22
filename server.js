788d52e2dab510636cbe71651c9496f004268942
Evan Yang <evanyang@pop-os.localdomain>
2020-04-21 22:42:55 -0700

    deployment stuff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const items = require('./routes/api/items')
 
@@ -20,6 +21,17 @@ require('./config').then(() => {
 
 // Use routes
 app.use('/api/items', items)
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'))
+
+    // any request 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () =>  console.log(`Server started on port ${port}`))
