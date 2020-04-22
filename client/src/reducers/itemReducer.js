32b360eeaf802242e40018b7c047a49d98580184
Evan Yang <evanyang@pop-os.localdomain>
2020-04-21 22:32:29 -0700

    connect to backend

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,29 +1,33 @@
 import {v1 as uuid} from 'uuid'
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types'
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types'
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'Eggs'},
-        { id: uuid(), name: 'Milk'},
-        { id: uuid(), name: 'Water'}
-    ]
+    items: [],
+    loading: false // once we make the request, set to true; once we get the data back, set to false again
 }
 
 export default function (state = initialState, action) {
     switch(action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload, // b/c items = [] initially
+                loading: false
             }
         case DELETE_ITEM:
             return {
                 ...state,
-                items: state.items.filter(item => item.id !== action.payload) //action.payload = id
+                items: state.items.filter(item => item._id !== action.payload) //action.payload = id
             }
         case ADD_ITEM: 
             return {
                 ...state,
                 items: [action.payload, ...state.items]
+            }
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
             }
         default: 
             return state
